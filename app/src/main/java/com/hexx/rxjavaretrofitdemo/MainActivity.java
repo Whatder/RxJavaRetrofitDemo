007809da84d152d0f0e098edd2f5601e887aabc4
何何何 <523035216@qq.com>
2018-01-24 11:00:13 +0800

    retrofit butterknife

--- app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
+++ app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
@@ -1,13 +1,21 @@
 package com.hexx.rxjavaretrofitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvResponse)
+    TextView tvResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
