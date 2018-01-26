6b339470b87ce014f63cb9f04418a2e9cb5fc45a
何何何 <523035216@qq.com>
2018-01-26 15:37:41 +0800

    top250榜单显示

--- app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
+++ app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
@@ -2,17 +2,23 @@ package com.hexx.rxjavaretrofitdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.hexx.rxjavaretrofitdemo.bean.DataBean;
 import com.hexx.rxjavaretrofitdemo.retrofit.ResponseApi;
+import com.hexx.rxjavaretrofitdemo.view.Top250Adapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,17 +27,31 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.tvResponse)
-    TextView tvResponse;
+    @BindView(R.id.top250recyclerView)
+    RecyclerView top250recyclerView;
+    @BindView(R.id.top250refreshLayout)
+    SmartRefreshLayout top250refreshLayout;
+    Top250Adapter top250Adapter;
+    List<DataBean.SubjectsBean> top250Data = new ArrayList<>();
+    private int start, count = 20;
+    private boolean canLoadMore = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
         getData();
+        initRefresh();
     }
 
+    private void initView() {
+        top250Adapter = new Top250Adapter(this, top250Data);
+        top250recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        top250recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        top250recyclerView.setAdapter(top250Adapter);
+    }
 
     private void getData() {
         //创建retrofit对象
@@ -43,19 +63,45 @@ public class MainActivity extends AppCompatActivity {
         ResponseApi responseApi = retrofit.create(ResponseApi.class);
 
         //调用接口-Gson解析
-        Call<DataBean> call1 = responseApi.getTop250(1, 5);
+        Call<DataBean> call1 = responseApi.getTop250(start, count);
         call1.enqueue(new Callback<DataBean>() {
             @Override
             public void onResponse(Call<DataBean> call, Response<DataBean> response) {
-                tvResponse.setText(response.body().getTitle()
-                        + "页数:" + response.body().getStart() + "/" + response.body().getTotal() + "页");
+                top250Data.addAll(response.body().getSubjects());
+                top250Adapter.notifyDataSetChanged();
+
+                if (response.body().getStart() < (response.body().getTotal() / response.body().getCount())) {
+                    start++;
+                    canLoadMore = true;
+                } else {
+                    canLoadMore = false;
+                }
             }
 
             @Override
             public void onFailure(Call<DataBean> call, Throwable t) {
-                tvResponse.setText(t.getMessage());
             }
         });
     }
 
+    private void initRefresh() {
+        top250refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                start = 0;
+                top250Data.clear();
+                getData();
+                refreshlayout.finishRefresh(2000);
+            }
+        });
+        top250refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                if (canLoadMore) {
+                    getData();
+                    refreshlayout.finishLoadmore(2000);
+                }
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/hexx/rxjavaretrofitdemo/view/Top250Adapter.java
@@ -0,0 +1,68 @@
+package com.hexx.rxjavaretrofitdemo.view;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.hexx.rxjavaretrofitdemo.R;
+import com.hexx.rxjavaretrofitdemo.bean.DataBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by HE on 2018/1/26.
+ */
+
+public class Top250Adapter extends RecyclerView.Adapter<Top250Adapter.ViewHolder> {
+    private Context context;
+    private LayoutInflater inflater;
+    private List<DataBean.SubjectsBean> data;
+
+    public Top250Adapter(Context context, List<DataBean.SubjectsBean> data) {
+        this.context = context;
+        this.data = data;
+        this.inflater = LayoutInflater.from(this.context);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.item_top250, null, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Glide.with(context).load(data.get(position).getImages().getLarge()).into(holder.icon);
+        holder.name.setText(data.get(position).getTitle() + "(" + data.get(position).getOriginal_title() + ")");
+        holder.summary.setText(data.get(position).getDirectors().get(0).getName() + data.get(position).getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.icon)
+        ImageView icon;
+        @BindView(R.id.name)
+        TextView name;
+        @BindView(R.id.summary)
+        TextView summary;
+        @BindView(R.id.item)
+        LinearLayout item;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
