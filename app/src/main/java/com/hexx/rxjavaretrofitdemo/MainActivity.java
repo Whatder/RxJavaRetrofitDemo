986b4177d3f2039b6954b90c0c8cd0b94fe8fa06
何何何 <523035216@qq.com>
2018-01-30 16:23:15 +0800

    recovery

--- app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
+++ app/src/main/java/com/hexx/rxjavaretrofitdemo/MainActivity.java
@@ -1,7 +1,5 @@
 package com.hexx.rxjavaretrofitdemo;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -9,7 +7,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.hexx.rxjavaretrofitdemo.bean.Top250Bean;
-import com.hexx.rxjavaretrofitdemo.bean.USBoxBean;
 import com.hexx.rxjavaretrofitdemo.retrofit.RetrofitHelper;
 import com.hexx.rxjavaretrofitdemo.retrofit.ServiceApi;
 import com.hexx.rxjavaretrofitdemo.view.Top250Adapter;
@@ -23,7 +20,6 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -63,84 +59,35 @@ public class MainActivity extends AppCompatActivity {
         //创建retrofit对象
         //结合rxjava
         ServiceApi service = RetrofitHelper.getService().create(ServiceApi.class);
-//        service.getTop250(start, count)
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<Result<Top250Bean>>() {
-//                    @Override
-//                    public void onSubscribe(Disposable d) {
-//                    }
-//
-//                    @Override
-//                    public void onNext(Result<Top250Bean> value) {
-//                        top250Data.addAll(value.response().body().getSubjects());
-//                        if (value.response().body().getStart() < (value.response().body().getTotal() / value.response().body().getCount())) {
-//                            start++;
-//                            canLoadMore = true;
-//                        } else {
-//                            canLoadMore = false;
-//                        }
-//
-//                        if (refreshlayout != null) {
-//                            if (refreshType == REFRESH) {
-//                                refreshlayout.finishRefresh();
-//                            } else if (refreshType == LOAD_MORE) {
-//                                refreshlayout.finishLoadmore();
-//                            }
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                        top250Adapter.notifyDataSetChanged();
-//                    }
-//                });
-
-        Observable top250Observable = service.getTop250(start, count);
-        Observable usboxObservable = service.getWeekly(start, count);
-
-        Observable.merge(top250Observable, usboxObservable)
+        service.getTop250(start, count)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer() {
+                .subscribe(new Observer<Result<Top250Bean>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
                     }
 
                     @Override
-                    public void onNext(Object value) {
-                        if (value instanceof Top250Bean) {
-                            top250Data.addAll(((Top250Bean) value).getSubjects());
-                            if (((Top250Bean) value).getStart() < ((Top250Bean) value).getTotal() / ((Top250Bean) value).getCount()) {
-                                start++;
-                                canLoadMore = true;
-                            } else {
-                                canLoadMore = false;
-                            }
+                    public void onNext(Result<Top250Bean> value) {
+                        top250Data.addAll(value.response().body().getSubjects());
+                        if (value.response().body().getStart() < (value.response().body().getTotal() / value.response().body().getCount())) {
+                            start++;
+                            canLoadMore = true;
+                        } else {
+                            canLoadMore = false;
+                        }
 
-                            if (refreshlayout != null) {
-                                if (refreshType == REFRESH) {
-                                    refreshlayout.finishRefresh();
-                                } else if (refreshType == LOAD_MORE) {
-                                    refreshlayout.finishLoadmore();
-                                }
+                        if (refreshlayout != null) {
+                            if (refreshType == REFRESH) {
+                                refreshlayout.finishRefresh();
+                            } else if (refreshType == LOAD_MORE) {
+                                refreshlayout.finishLoadmore();
                             }
-                        } else if (value instanceof USBoxBean) {
-                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                            alertDialogBuilder.setTitle(((USBoxBean) value).getTitle()).setMessage(((USBoxBean) value).getDate())
-                                    .setPositiveButton("确定", null)
-                                    .setNegativeButton("取消", null).show();
                         }
                     }
 
                     @Override
                     public void onError(Throwable e) {
-
                     }
 
                     @Override
@@ -148,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
                         top250Adapter.notifyDataSetChanged();
                     }
                 });
+
 
     }
 
--- app/src/main/java/com/hexx/rxjavaretrofitdemo/retrofit/ServiceApi.java
+++ app/src/main/java/com/hexx/rxjavaretrofitdemo/retrofit/ServiceApi.java
@@ -2,9 +2,9 @@ package com.hexx.rxjavaretrofitdemo.retrofit;
 
 
 import com.hexx.rxjavaretrofitdemo.bean.Top250Bean;
-import com.hexx.rxjavaretrofitdemo.bean.USBoxBean;
 
 import io.reactivex.Observable;
+import retrofit2.adapter.rxjava2.Result;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -16,9 +16,6 @@ public interface ServiceApi {
     String BaseUrl = "http://api.douban.com/";
 
     @GET("/v2/movie/top250")
-    Observable<Top250Bean> getTop250(@Query("start") int start, @Query("count") int count);
-
-    @GET("/v2/movie/us_box")
-    Observable<USBoxBean> getWeekly(@Query("start") int start, @Query("count") int count);
+    Observable<Result<Top250Bean>> getTop250(@Query("start") int start, @Query("count") int count);
 
 }
